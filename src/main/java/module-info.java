36105780941712777cd3a3cd93fbad24bec21f9f
Omar <1omerkhaled1@gmail.com>
2022-12-14 22:15:26 +0200

    lecturer gui

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.example.guios {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.kordamp.bootstrapfx.core;
+
+    opens com.example.guios to javafx.fxml;
+    exports com.example.guios;
+
+
+}
